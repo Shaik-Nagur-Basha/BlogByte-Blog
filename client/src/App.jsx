c1d6c342c5b4b70353dc1c6bdf4657de62b42e85
Shaik Nagur Basha <sknbasknba@gmail.com>
2024-09-23 16:25:09 +0530

    minor changes to previous commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,8 +14,8 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
