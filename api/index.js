74dceb03a94e2fcd1f9fb6a0710be7c5618cad0d
Shaik Nagur Basha <sknbasknba@gmail.com>
2024-09-24 00:01:15 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("MongoDB is Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
@@ -6,6 +19,6 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
 
-app.get('/get', (req, res) => {
-    res.send("You Got it!")
-})
+app.get("/get", (req, res) => {
+  res.send("You Got it!");
+});
