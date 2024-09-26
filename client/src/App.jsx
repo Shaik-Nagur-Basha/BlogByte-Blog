39c188ee9467e4f1345413ea5f4945489c326290
Shaik Nagur Basha <sknbasknba@gmail.com>
2024-09-26 16:20:38 +0530

    create and add the footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignUp from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import FooterCom from "./components/Footer";
 
 export default function App() {
   return (
@@ -20,6 +21,7 @@ export default function App() {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
+      <FooterCom/>
     </BrowserRouter>
   );
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,97 @@
+import { Footer } from "flowbite-react";
+import { Link } from "react-router-dom";
+import {
+  BsFacebook,
+  BsGithub,
+  BsInstagram,
+  BsTelegram,
+  BsTwitter,
+} from "react-icons/bs";
+
+export default function FooterCom() {
+  return (
+    <Footer container className="border border-t-8 border-teal-500">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+            >
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                Freelancer
+              </span>
+              Blog
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="About" />
+              <Footer.LinkGroup className="grid gap-3">
+                <Footer.Link
+                  href="https://listing-hub.onrender.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  My First Project
+                </Footer.Link>
+                <Footer.Link
+                  href="https://listing-hub.onrender.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Freelancer Blog
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow us" />
+              <Footer.LinkGroup className="grid gap-3">
+                <Footer.Link
+                  href="https://github.com/Shaik-Nagur-Basha"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </Footer.Link>
+                <Footer.Link
+                  href="https://t.me/sknba"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Telegram
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup className="grid gap-3">
+                <Footer.Link href="#" target="_blank" rel="noopener noreferrer">
+                  Privacy Policy
+                </Footer.Link>
+                <Footer.Link href="#" target="_blank" rel="noopener noreferrer">
+                  Terms &amp; Conditions
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="Freelancer Blog"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 mt-4 sm:mt-0 sm:justify-center">
+            <Footer.Icon href="#" icon={BsGithub} />
+            <Footer.Icon href="#" icon={BsTelegram} />
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsTwitter} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
