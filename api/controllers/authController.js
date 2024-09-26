2ccf47800e99f9245a9eb70fd7b3e50d67fec887
Shaik Nagur Basha <sknbasknba@gmail.com>
2024-09-26 23:39:35 +0530

    create signin api route

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,6 +1,7 @@
 import User from "../models/userModel.js";
 import bcryptjs from "bcrypt";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   let { username, email, password } = req.body;
@@ -26,6 +27,36 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.json("Sign-up Successful");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  let { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(400, "User not found")); //We can use same error message for INCORRECT email For "High security"
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid Password")); //We can use same error message for INCORRECT email For "High security"
+    }
+
+    const { password: pass, ...rest } = validUser._doc;
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(rest);
   } catch (err) {
     next(err);
   }
