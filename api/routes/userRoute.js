94a9fb2fb23e98bd6f4ed00c5d65a65c252a746b
Shaik Nagur Basha <sknbasknba@gmail.com>
2024-10-02 22:31:11 +0530

    show the comments of a post

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   deleteUser,
+  getUser,
   getUsers,
   signout,
   test,
@@ -14,6 +15,7 @@ router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
-router.get('/getusers', verifyToken, getUsers)
+router.get("/getusers", verifyToken, getUsers);
+router.get("/:ownerId", getUser);
 
 export default router;
