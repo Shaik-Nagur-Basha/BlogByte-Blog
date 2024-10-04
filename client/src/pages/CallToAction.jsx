50a5d6f0d046b5cf69a9e1b562c9d42673418923
Shaik Nagur Basha <sknbasknba@gmail.com>
2024-10-04 16:11:01 +0530

    complete the home page

--- client/src/pages/CallToAction.jsx
+++ client/src/pages/CallToAction.jsx
@@ -2,15 +2,17 @@ import { Button } from "flowbite-react";
 
 export default function CallToAction() {
   return (
-    <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
-      <div className="felx-1 justify-center flex flex-col">
-        <h2 className="text-2xl">Want to learn more about javascript?</h2>
-        <p className="text-gray-500 my-2">
-          Checkout these resources with 100 JS Practice's{" "}
+    <div className="flex flex-col max-w-fit mx-auto lg:flex-row gap-8 border border-teal-500 p-8 rounded-tl-3xl rounded-br-3xl calltoaction">
+      <div className="flex flex-col justify-center items-center">
+        <h2 className="text-lg font-semibold sm:text-2xl dark:text-white">
+          Want to learn more about javascript?
+        </h2>
+        <p className="text-gray-500 my-2 text-xs sm:text-sm">
+          Checkout these resources with 100 JS Practice's
         </p>
         <Button
           gradientDuoTone="purpleToPink"
-          className="rounded-tl-xl rounded-bl-none"
+          className="rounded-tl-xl rounded-bl-none w-2/4 mt-5"
         >
           <a
             href="https://www.interviewbit.com/"
@@ -21,10 +23,11 @@ export default function CallToAction() {
           </a>
         </Button>
       </div>
-      <div className="p-7 flex-1">
+      <div className="flex justify-center items-center">
         <img
+          className="max-h-72 max-w-96 object-cover p-4"
           src="https://miro.medium.com/v2/resize:fit:700/1*WKk1knUrSfH-Inlkg0kEMw.png"
-          alt=""
+          alt="image"
         />
       </div>
     </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,58 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import CallToAction from "./CallToAction";
+import PostCard from "../components/PostCard";
+
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    try {
+      const fetchPosts = async () => {
+        const res = await fetch("/api/post/getPosts");
+        const data = await res.json();
+        setPosts(data.posts);
+      };
+      fetchPosts();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
   return (
-    <div>Home</div>
-  )
+    <div className="">
+      <div className="flex flex-col flex-nowrap gap-5 max-w-full md:p-28 p-5 my-7">
+        <h1 className="text-3xl font-bold md:text-5xl">
+          Welcome to freelancer Blog
+        </h1>
+        <p className="text-xs md:text-sm text-gray-500">
+          Here you'll find a variety of articles and tutorials on topics such as
+          web development, software engineering, and programming languages.
+        </p>
+        <Link
+          to={"/search"}
+          className="text-teal-500 hover:underline font-bold w-fit"
+        >
+          View all posts
+        </Link>
+      </div>
+      <div className="home bg-amber-50 p-1 dark:bg-[#334155]">
+        <CallToAction />
+      </div>
+      <div className="my-12">
+        <h2 className="text-2xl font-bold text-center mb-5">Recent Posts</h2>
+        <div className="flex flex-wrap lg:gap-10 gap-4 justify-center p-4">
+          {posts.map((post) => (
+            <PostCard key={post._id} post={post} />
+          ))}
+        </div>
+        <div className="flex justify-center items-center mt-4">
+          <Link
+            to={"/search"}
+            className="text-teal-500 hover:underline font-bold w-fit"
+          >
+            View all posts
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
