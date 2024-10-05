718304b2e688f36c20df4119e278162870e704cd
Shaik Nagur Basha <sknbasknba@gmail.com>
2024-10-05 17:45:37 +0530

    error's solved!

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { persistor, store } from "./redux/store.js";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import ThemeProvider from "./components/themeProvider";
+import ThemeProvider from "./components/ThemeProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <PersistGate persistor={persistor}>
